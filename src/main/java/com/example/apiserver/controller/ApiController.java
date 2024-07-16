c743d78fcbffae36f50b97095ecafa61f1a86302
DOVB <gtoyoung@naver>
2024-07-16 22:54:31 +0900

    schedule api 노출 제거

--- src/main/java/com/example/apiserver/controller/ApiController.java
+++ src/main/java/com/example/apiserver/controller/ApiController.java
@@ -10,24 +10,24 @@ import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@Api(tags = {"해외 축구 기사 API"})
+@Api(tags = {"해축 API"})
 public class ApiController {
 
     private final NewsService service;
 
-
-
     @GetMapping(value = "/newsList")
-    @ApiOperation(value = "해외 축구 기사", response = SoccerNews.class)
+    @ApiOperation(value = "해축 Article", response = SoccerNews.class)
     public Page<SoccerNews> getNews(@RequestParam(required = false, defaultValue = "") String search, @RequestParam(required = false, defaultValue = "") int page, @RequestParam(required = false, defaultValue = "") int size, @RequestParam(required = false, defaultValue = "") String date) {
         return service.getNewsList(search, page, size, date);
     }
 
+    @ApiIgnore
     @GetMapping(value = "/schedule")
     public String getSchedule() throws ParseException {
         service.deleteNews();
